847cb30a88c7086d321d0f67f1b2e01f6c9bca42
Neuman Vong <neuman@twilio.com>
2011-01-18 10:17:30 -0800

    Add handler for receiving phone calls

--- ogori.py
+++ ogori.py
@@ -3,9 +3,20 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 
+BASE_URL = 'http://ogori-masu.appspot.com'
 class MainPage(webapp.RequestHandler):
     """Plays a message if one exists, then <Record>s a new one."""
-    pass
+    def get(self):
+        self.response.out.write(
+                '<?xml version="1.0" encoding="UTF-8"?><Response>')
+        rec = Recording.get_next()
+        if rec is not None:
+            self.response.out.write('<Play>%s</Play>' % rec.recording_url)
+        url = BASE_URL + '/record'
+        self.response.out.write('''\
+                <Record action="%s"/>
+                </Response>''' % url
+                )
 
 class RecordPage(webapp.RequestHandler):
     """Saves the URL of the recorded message."""
